884939e7afea952d8fc8a73cedf4814518a2fab4
Robin Hotton <Robin.hotton@hotmail.com>
2023-01-16 22:05:51 +0100

    refactor

--- src/services/RequetesAxios.ts
+++ src/services/RequetesAxios.ts
@@ -1,13 +1,20 @@
 import axios from "axios";
 import dotenv from "dotenv";
-dotenv.config();
+
+let URL: string;
+
+const initializer = () => {
+  dotenv.config();
+  URL = "" + process.env.API_URL;
+};
 
 /**
  * cible la route /prestations pour récupérer des données de prestations
  */
 export const getPrestations = async () => {
+  initializer();
   try {
-    const response = await axios.get(`${process.env.API_URL}/prestations`);
+    const response = await axios.get(`${URL}/prestations`);
     return response.data;
   } catch (error) {
     return "error at: RequeteAxios.ts -> getPrestations -> catch";
@@ -18,8 +25,9 @@ export const getPrestations = async () => {
  *  cible la route /categories pour récupérer des données de catégories
  */
 export const getCategories = async () => {
+  initializer();
   try {
-    const response = await axios.get(`${process.env.API_URL}/categories`);
+    const response = await axios.get(`${URL}/categories`);
     return response.data;
   } catch (error) {
     return "error at: RequeteAxios.ts -> getCategories -> catch";
@@ -30,8 +38,9 @@ export const getCategories = async () => {
  *  cible la route /entreprises pour récupérer des données de catégories
  */
 export const getEntreprises = async () => {
+  initializer();
   try {
-    const response = await axios.get(`${process.env.API_URL}/entreprises`);
+    const response = await axios.get(`${URL}/entreprises`);
     return response.data;
   } catch (error) {
     return "error at: RequeteAxios.ts -> getEntreprises -> catch";
@@ -42,8 +51,9 @@ export const getEntreprises = async () => {
  *  cible la route /clients pour récupérer des données de catégories
  */
 export const getClients = async () => {
+  initializer();
   try {
-    const response = await axios.get(`${process.env.API_URL}/clients`);
+    const response = await axios.get(`${URL}/clients`);
     return response.data;
   } catch (error) {
     return "error at: RequeteAxios.ts -> getClients -> catch";

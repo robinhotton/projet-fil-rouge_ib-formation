4f2f227c2f11d88ce397514ee0acf0a161646779
Robin Hotton <Robin.hotton@hotmail.com>
2023-01-17 15:14:20 +0100

    service fetch get

--- src/components/card/CardsController.tsx
+++ src/components/card/CardsController.tsx
@@ -1,12 +1,12 @@
+import GetPrestations from "../../services/GetPrestations";
 import CardsCategorie from "./CardsCategorie";
 import StatsGlobales from "./CardsGlobales";
-import { getPrestations, getCategories } from "../../services/RequetesAxios";
 
 const CardsController: React.FC = () => {
   return (
     <>
       <h1>Statistiques globales</h1>
-      <StatsGlobales getData={getPrestations} />
+      <StatsGlobales getData={GetPrestations} />
       <h1>Prestations</h1>
       <div className="allCategories">
         {/* {getCategories.map((categorie: any, index: number) => (

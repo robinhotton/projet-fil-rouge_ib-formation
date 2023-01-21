e6fec49880fa13ad19d8c18568aa033edbc5d789
Robin Hotton <Robin.hotton@hotmail.com>
2023-01-21 10:02:05 +0100

    feat: gridCategorie

--- src/components/gridCategorie/GridCategorie.tsx
+++ src/components/gridCategorie/GridCategorie.tsx
@@ -2,16 +2,36 @@ import "./GridCategorie.scss";
 
 const GridCategorie: React.FC = () => {
   return (
-    <>
-      <div className="wrapper">
-        <div className="one">One</div>
-        <div className="two">Two</div>
-        {/* <div className="three">Three</div>
-        <div className="four">Four</div>
-        <div className="five">Five</div>
-        <div className="six">Six</div> */}
-      </div>
-    </>
+    <main className="categorie-grid">
+      <article className="categorie grid-col-span-2">
+        <img
+          src="https://journal-electricite.com/wp-content/uploads/2021/10/schema-de-cablage-tableau-electrique.jpg"
+          alt="Le câblage d'armoire électrique"
+        />
+        <p>Le câblage d'armoire électrique</p>
+      </article>
+      <article className="categorie">
+        <img
+          src="https://www.allnews.ch/sites/default/files/images/Poignee_Main_CS_Barings_384043599_Keystone_RS.jpg"
+          alt="L'accompagnement aux choix de matériels industriels"
+        />
+        <p>L'accompagnement aux choix de matériels industriels</p>
+      </article>
+      <article className="categorie">
+        <img
+          src="https://www.automatisme-vision-service.com/oktThemes/ra160-s/images/integ/image2.jpg"
+          alt="L'installation de matériels"
+        />
+        <p>L'installation de matériels</p>
+      </article>
+      <article className="categorie">
+        <img
+          src="https://www.alternance-professionnelle.fr/wp-content/uploads/2017/09/emploi-secteur-BTP.jpg"
+          alt="Autre"
+        />
+        <p>Autre</p>
+      </article>
+    </main>
   );
 };
 export default GridCategorie;

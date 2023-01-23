bb27965e9d1f42f3b09d18ed70f18b94b92384d1
Robin Hotton <Robin.hotton@hotmail.com>
2023-01-23 21:48:50 +0100

    préparation création de clients

---
+++ src/models/Client.ts
@@ -0,0 +1,17 @@
+import Prestation from "./Prestation";
+
+export default class Client {
+  id?: number;
+  nom: string;
+  prenom: string;
+  mail: string;
+  motDePasse: string;
+  panier: Prestation[] = [];
+
+  constructor(nom: string, prenom: string, mail: string, motDePasse: string) {
+    this.nom = nom;
+    this.prenom = prenom;
+    this.mail = mail;
+    this.motDePasse = motDePasse;
+  }
+}

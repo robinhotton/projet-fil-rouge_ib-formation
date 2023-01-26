6ad74cc63cc8fd9d4257551883bbdd18bec198da
Robin Hotton <Robin.hotton@hotmail.com>
2023-01-26 21:08:00 +0100

    ajout image landingPage et modification du bouton du formulaire :lipstick:

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,11 +2,17 @@ import Footer from "../commons/Footer/Footer";
 import Header from "../commons/header/Header";
 import GridCategorie from "../components/gridCategorie/GridCategorie";
 import LandingForm from "../components/landingForm/LandingForm";
+import "./LandingPage.scss";
 
 const LandingPage: React.FC = () => {
   return (
     <>
       <Header />
+      <img
+        className="image-LandingPage"
+        src="https://s7d2.scene7.com/is/image/Caterpillar/CM20170110-43489-17107"
+        alt="chantier btp"
+      />
       <LandingForm />
       <GridCategorie />
       <Footer />

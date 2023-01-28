fec4dec87ee6aa6ab27dc625e48ea313f21937eb
Robin Hotton <Robin.hotton@hotmail.com>
2023-01-28 08:15:16 +0100

    style: fix footer en bas de la page :lipstick:

--- src/pages/CreatePrestationPage.tsx
+++ src/pages/CreatePrestationPage.tsx
@@ -1,14 +1,14 @@
 import Footer from "../commons/Footer/Footer";
 import Header from "../commons/header/Header";
 import CreatePrestation from "../components/prestationForm/createPrestationForm";
-
+import "./CreatePrestationPage.scss";
 const CreatePrestationPage: React.FC = () => {
   return (
-    <>
+    <div className="FixFooterBas">
       <Header />
       <CreatePrestation />
       <Footer />
-    </>
+    </div>
   );
 };
 export default CreatePrestationPage;
--- src/pages/EditPrestationPage.tsx
+++ src/pages/EditPrestationPage.tsx
@@ -1,14 +1,15 @@
 import Footer from "../commons/Footer/Footer";
 import Header from "../commons/header/Header";
 import EditPrestation from "../components/prestationForm/EditPrestationForm";
+import "./CreatePrestationPage.scss";
 
 const EditPrestationPage: React.FC = () => {
   return (
-    <>
+    <div className="FixFooterBas">
       <Header />
       <EditPrestation />
       <Footer />
-    </>
+    </div>
   );
 };
 export default EditPrestationPage;

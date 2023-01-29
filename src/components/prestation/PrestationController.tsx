659cd46c2d37ff1315aedbb8ef0acfb71526af45
PierreD59 <pierre.dmt59@gmail.com>
2023-01-29 18:29:06 +0100

    Fin des modifications css sur toutes les pages

--- src/components/prestation/PrestationController.tsx
+++ src/components/prestation/PrestationController.tsx
@@ -36,7 +36,7 @@ const PrestationController: React.FC = () => {
 
   return (
     <>
-      <h1 className="categorieTitre">Catégorie : {categorie?.nom}</h1>
+      <h1 className="titleCategory bold">Catégorie : {categorie?.nom}</h1>
       <div className="categories">
         {prestations.length > 0 ? (
           <>
